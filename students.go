3a03442741e6a06b6d078686d021391a6283e6e3
phbeauch <phbeauch@gmail.com>
2015-11-04 14:30:35 -0500

    Create students.go

---
+++ students.go
@@ -0,0 +1,11 @@
+package main
+
+type Student struct {
+	NetID	string		`json: "id"		bson: "_id"`
+	Name	string		`json: "name"	bson: "name"`
+	Major	string		`json: "major"	bson: "major"`
+	Year	int			`json: "year"	bson: "year"`
+	Grade	int			`json: "grade"	bson: "grade"`
+	Rating	string		`json: "rating"	bson: "rating"`
+}
+type Students []Student
