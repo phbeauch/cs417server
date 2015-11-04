dbd4a1d969dde5d9c13ac19b977f41709411ce35
phbeauch <phbeauch@gmail.com>
2015-11-04 14:28:58 -0500

    Create router.go

---
+++ router.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func NewRouter() *mux.Router {
+    router:=mux.NewRouter().StrictSlash(true)
+    for _,route:=range routes {
+        var handler http.Handler
+        handler=route.HandlerFunc
+        handler=Logger(handler, route.Name)
+        if route.Name=="GetStudent" {
+          router.
+            Methods(route.Method).
+            Path(route.Pattern).
+            Name(route.Name).
+            Queries("name","{name}").
+            Handler(handler)
+        } else{
+          router.
+            Methods(route.Method).
+            Path(route.Pattern).
+            Name(route.Name).
+            Handler(handler)
+        }
+    }
+    return router
+}
